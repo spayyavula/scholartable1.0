93b3f839099fcf7f83b158aa865f92ccdd0ff50b
Sreekanth <spayyavula@gmail.com>
2025-06-21 08:18:32 -0700

    Fix TensorFlow module resolution error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops'
+      ]
+    }
+  },
   optimizeDeps: {
     exclude: [
       'lucide-react'
